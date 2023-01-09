f51f038fa4b0711034d8e49a41b4b050252f5fac
Bruno Mogetta <brunomogetta@gmail.com>
2023-01-09 10:54:23 -0300

    test

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -8,4 +8,4 @@ echo "fin de echo"
 git config --local user.email "action@github.com"
 git config --local user.name "Github Action"
 git add -A
-git diff-index --quiet HEAD || (git commit -a -m "docs: Added $COMMIT_NAME" --allow-empty)
+git diff-index --quiet HEAD || (git commit -a -m "docs: Added $value" --allow-empty)
