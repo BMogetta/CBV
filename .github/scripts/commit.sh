fd613b576cbed96f7b009f9569cc68958516147f
Bruno Mogetta <brunomogetta@gmail.com>
2023-01-09 10:18:35 -0300

    test

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -1,8 +1,9 @@
-#!bin/sh
+#!/usr/bin/env bash
 
-git config --local user.email "action@github.com"
 echo $NEW_CBV_NAME
 echo $COMMIT_NAME
+
+git config --local user.email "action@github.com"
 git config --local user.name "Github Action"
 git add -A
 git diff-index --quiet HEAD || (git commit -a -m "docs: Added $COMMIT_NAME" --allow-empty)
