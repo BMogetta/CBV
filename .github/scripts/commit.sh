232a070700e84ede13ca7382ff25c4d8ff23d197
Bruno Mogetta <brunomogetta@gmail.com>
2023-01-09 15:08:40 -0300

    test

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-echo "inicio de echo"
 { IFS= read -rd '' value <DENO_OUTPUT.txt;} 2>/dev/null
-printf '%s' "$value"
-echo "fin de echo"
-
 
 git config --local user.email "action@github.com"
 git config --local user.name "Github Action"
