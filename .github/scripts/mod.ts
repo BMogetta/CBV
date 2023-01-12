a1e04c409fe3e1400a0e1b6eb6517b76f8a988af
Bruno Mogetta <brunomogetta@gmail.com>
2023-01-12 12:12:31 -0300

    strng to parse

--- .github/scripts/mod.ts
+++ .github/scripts/mod.ts
@@ -14,7 +14,7 @@ main(data_given_by_gh)
 async function main(args: Array<string>) {
   
   // LABELS
-  const issue_labels = JSON.parse(args[0]);
+  const issue_labels = JSON.parse(JSON.stringify(args[0]));
   if (!issue_labels.find( (acp) => acp.name === "Accepted")) {
     // Because this exit here, no changes are made, and no code is ever pushed
     Deno.exit(0)
